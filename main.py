aeec5fca5cea772fa6fd125c403a6759e950015b
hgorski6 <41839752+hgorski6@users.noreply.github.com>
2022-06-22 20:12:01 +0100

    Conversion and compression done, command line use still needs work

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from PIL import Image
 import numpy as np
 import sys
 
-file_in = repr(sys.stdin[0])
-file_out = repr(sys.stdin[1])
-comp = int(sys.stdin[2])
+# file_in = repr(sys.argv[1])
+# file_out = repr(sys.argv[2])
+# comp = int(sys.argv[3])
+#
+# print(file_in)
+# print(file_out)
+# print(comp)
 
-# file_in = r"C:\Users\huber\PycharmProjects\ImageToASCII\Test Folder In\Gardevoir.jpg"
-# file_out = r"C:\Users\huber\PycharmProjects\ImageToASCII\Test Folder Out\Gardevoir_ASCII.txt"
-# comp = 3
+file_in = r"C:\Users\huber\PycharmProjects\ImageToASCII\Test Folder In\Gardevoir.jpg"
+file_out = r"C:\Users\huber\PycharmProjects\ImageToASCII\Test Folder Out\Gardevoir_ASCII.txt"
+comp = 1
 
 im = Image.open(file_in)
 pix = im.load()
@@ -16,16 +20,13 @@ pix = im.load()
 data_write = open(file_out, "w")
 data = open(file_out)
 
-
 def big_pix(pix, size, x, y):
-    pix_sum = [0, 0, 0]
+    pix_sum = np.array([0, 0, 0])
     for i in range(x, x + size):
-        for n in range(y, y + size):
-            pix_sum[0] += pix[x, y][0]
-            pix_sum[1] += pix[x, y][1]
-            pix_sum[2] += pix[x, y][2]
-    pix_mean = np.array(pix_sum) // size ** 2
-    return tuple(pix_mean)
+        for k in range(y, y + size):
+            pix_sum = + np.array(pix[min(i, im.height - 1), min(k, im.width - 1)])
+    pix_mean = pix_sum / size ** 2
+    return pix_mean
 
 
 # def ASCIIconversion(r, g, b):
@@ -34,12 +35,14 @@ def big_pix(pix, size, x, y):
 for y in range(0, im.size[1], comp):
     image_row = []
     for x in range(0, im.size[0], comp):
-        r = big_pix(pix, comp, x, y)[0]
-        g = big_pix(pix, comp, x, y)[1]
-        b = big_pix(pix, comp, x, y)[2]
-        conv = int((r + g + b)) // 6
-        pixel_ascii = chr(max(conv, 50))
+        conv = 50 + int(sum(big_pix(pix, comp, x, y)) // 6)
+        pixel_ascii = chr(conv)
+        if conv >= 86:
+            pixel_ascii = "."
+        # if conv in list(range(7, 14)) or conv in range(30, 34):
+        #     pixel_ascii = chr(conv + 7)
         image_row.append(" " + str(pixel_ascii))
-    data_write.write("".join(image_row) + "\n")
+    ascii_line = "".join(image_row) + "\n"
+    data_write.write(ascii_line)
 
 data.close()
