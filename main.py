81e810b2adf6a28f42dce2244776aeb7a4ef7cc1
hgorski6 <41839752+hgorski6@users.noreply.github.com>
2022-06-24 16:20:44 +0100

    Improved ASCII conversion

--- main.py
+++ main.py
@@ -20,27 +20,32 @@ pix = im.load()
 data_write = open(file_out, "w")
 data = open(file_out)
 
-def big_pix(pix, size, x, y):
+
+def big_pix(x, y):
     pix_sum = np.array([0, 0, 0])
-    for i in range(x, x + size):
-        for k in range(y, y + size):
+    for i in range(x, x + comp):
+        for k in range(y, y + comp):
             pix_sum = + np.array(pix[min(i, im.height - 1), min(k, im.width - 1)])
-    pix_mean = pix_sum / size ** 2
+    pix_mean = pix_sum / comp ** 2
     return pix_mean
 
 
-# def ASCIIconversion(r, g, b):
-
+# pix_actual = map(big_pix, list(range(im.height)), list(range(im.width)))
 
 for y in range(0, im.size[1], comp):
     image_row = []
     for x in range(0, im.size[0], comp):
-        conv = 50 + int(sum(big_pix(pix, comp, x, y)) // 6)
+        final_col = np.array([0, 0, 0])
+        i = 0
+        for col in big_pix(x, y):
+            squeeze = col * int(col > 85) * (85 / 255)
+            # forbidden = col * int(col in range(7, 14) or col == 32)
+            final_col[i] = squeeze + col * int(col <= 85)
+            i = + 1
+        conv = int(sum(final_col) // 3)
+        while conv in list(range(7, 14)) or conv == 32:
+            conv = conv + 1
         pixel_ascii = chr(conv)
-        if conv >= 86:
-            pixel_ascii = "."
-        # if conv in list(range(7, 14)) or conv in range(30, 34):
-        #     pixel_ascii = chr(conv + 7)
         image_row.append(" " + str(pixel_ascii))
     ascii_line = "".join(image_row) + "\n"
     data_write.write(ascii_line)
